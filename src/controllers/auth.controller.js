2e049e9ebee9cc744f737d0a682a9922a4d7c59c
Huguito038 <hugosoler@hotmail.com>
2023-10-09 20:16:39 -0300

    adding lax-cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const login = async(req,res)=>{
 
         const token = await CrearToken({id:userFound.id})
         res.cookie("token",token,{
-            sameSite: "none",
+            sameSite: "lax",
             secure: true, // Cambiado a false si no estás utilizando HTTPS
             httpOnly: false
         })
