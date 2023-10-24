078ddeb9927fe1beb0e96fa4b5b9835fb805b1da
Huguito038 <hugosoler@hotmail.com>
2023-10-24 16:06:11 -0300

    fix cors

--- cliente/src/Componentes/Redux/DefaultAxios.js
+++ cliente/src/Componentes/Redux/DefaultAxios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL:"http://localhost:4000/api"
+  baseURL:"https://clubpro-gestor.onrender.com/api"
 });
 
 export default instance
